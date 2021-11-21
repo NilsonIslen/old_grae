15ed43064d6b5e5685482c3db1c098a37a4bbefc
Islen <nilislen@gmail.com>
2021-11-21 12:01:05 -0500

    se cambia titulo de cabecera

--- head.php
+++ head.php
@@ -6,6 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="Stl.css" rel="stylesheet" type="text/css"> 
     <link  rel="icon" href="Imgs/logo.png" type="image/png" />
-    <title> Rutas AGD </title>
+    <title> Rutas Arepas El Grano Dorado </title>
 </head>
 <body>
