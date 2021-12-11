b666f4948ddbf2b4ec44970e7e8f5719023c976b
Islen <nilislen@gmail.com>
2021-12-10 21:02:13 -0500

    se retoma el naranja como color principal en los botones

--- index.php
+++ index.php
@@ -1016,7 +1016,7 @@ if(isset($_POST['Pedido'])){
                     }
 
                     if($IdVendedor<>0){
-                    echo "<p class='p_orange'>  Asignado a $NameVendedor </p>";
+                    echo "<p class='p_green'>  Asignado a $NameVendedor </p>";
                     echo "</div>";
                     }
                     }}}}}
