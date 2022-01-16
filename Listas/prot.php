3e943caa90e3100e8ef49caf85c158c067e20efc
Islen <nilislen@gmail.com>
2022-01-16 11:22:30 -0500

    se actualizan instrucciones de atencion al cliente

--- Listas/prot.php
+++ Listas/prot.php
@@ -11,13 +11,15 @@
         <h2> ¡Importante! </h2>
     <ol>
         <li class="ol-li"> Si no puedes establecer contacto con el cliente <br> NO envies el formulario, selecciona CANCELAR para que el cliente cuente con otra visita.</li>
-        <li class="ol-li"> Saluda cortezmente al entrar al establecimiento </li>
-        <li class="ol-li"> Revisa las fechas y el estado del producto </li>
-        <li class="ol-li"> Si el producto no es facilmente accesible consulta con el tendero como esta de fecha </li>
-        <li class="ol-li"> Si el producto del cliente esta vencido o su estado no es ideal realiza el cambio por producto fresco </li>
-        <li class="ol-li"> Consulta cuantos desea comprar </li>
-        <li class="ol-li"> Entrega el pedido y realiza el cobro correspondiente </li>
-        
+        <li class="ol-li"> Al llegar donde el cliente saluda cortezmente e indica que vas de parte  de <b> Arepas El Grano Dorado</b>. </li>
+        <li class="ol-li"> Revisa las fechas y el estado del producto. </li>
+        <li class="ol-li"> Si el producto no es facilmente accesible consulta con el tendero como esta de fecha. </li>
+        <li class="ol-li"> Consulta cuantos desea comprar. </li>
+        <li class="ol-li"> Entrega el pedido y realiza el cobro correspondiente. </li>
+        <li class="ol-li"> Registra las cantidades entregadas al cliente en los campos respectivos. </li>
+        <li class="ol-li"> Si el cliente tiene producto vencido o en mal estado selecciona "Si" en la ultima casilla.</li>  
+        <li class="ol-li"> Envia el formulario.</li>     
+        <li class="ol-li"> Recuerda que no debes visitar los clientes que esperan visita para cambio de producto ya que no es una venta, tenemos personal encargado para esta tarea. </li>  
     </ol>
         
 
